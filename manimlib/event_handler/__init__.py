c136c9618cb635f83832a99908da897787e8152e
LiJY136688 <150437199+LiJY136688@users.noreply.github.com>
2024-02-17 10:17:35 +0800

    Update __init__.py

--- manimlib/event_handler/__init__.py
+++ manimlib/event_handler/__init__.py
@@ -1,6 +1,4 @@
 from manimlib.event_handler.event_dispatcher import EventDispatcher
 
-
-# This is supposed to be a Singleton
-# i.e., during runtime there should be only one object of Event Dispatcher
+# 这应该是一个单例，即在运行期间应该只有一个事件调度对象
 EVENT_DISPATCHER = EventDispatcher()
